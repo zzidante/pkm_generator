a2917f046636890eab888efb2942bb52b215ae11
Mel <zzidante@gmail.com>
2018-03-31 11:22:39 -0700

    eslint and eshint config

---
+++ helpers/capitalize.js
@@ -0,0 +1,5 @@
+module.exports = 
+  String.prototype.Capitalize = function(lower) {
+    return (lower ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  // https://stackoverflow.com/questions/2332811/capitalize-words-in-string
+};
--- transform_pokemon_data.js
+++ transform_pokemon_data.js
@@ -1,33 +1,47 @@
+/*jslint node: true */
+'use strict';
+
 const insertAt = require('./helpers/insert_at.js');
+const capitalize = require('./helpers/capitalize.js');
 
 module.exports = {
-    getStats: function (pokemon, stat) {
-      if (stat) {
-        const lStat = stat.toLowerCase();
-        return { [lStat]: pokemon.stats[lStat] };
-      }
-      return pokemon.stats;
-    },
-    getPrettyStats: function (pokemon) {
-      const stats = this.getStats(pokemon);
-      let prettyStats = '';
-      prettyStats += pokemon.name + ' ';
-  
-      for (let stat in stats) {
-        prettyStats += stats[stat] + ' ';
-      }
-      return prettyStats;
-    },
-    buildGeneratorQuery: function(pokemon, lvl) {
-      if (lvl < 1 || lvl > 100) {
-        return 'Level given must be between 1 and 100.';
-      }
-      const stats = this.getPrettyStats(pokemon);
-      return stats.InsertAt(lvl + ' ', pokemon.name.length + 1);
-    },
-    getValidMoveName: function(pokemon, lvl) {
-      return pokemon.moves
-        .filter(move => move.level <= lvl )
-        .map(move => move.name );
-    },
+  getStats: function  (pokemon, stat) {
+    return (
+      stat ? 
+        { [stat.toLowerCase()]: pokemon.stats[stat.toLowerCase()] }
+        : pokemon.stats
+    );
+  },
+  getPrettyStats: function (pokemon) {
+    const stats = this.getStats(pokemon);
+    let prettyStats = pokemon.name + ' ';
+
+    for (let stat in stats) {
+      prettyStats += stats[stat] + ' ';
+    }
+    return prettyStats;
+  },
+  buildGeneratorQuery: function (pokemon, lvl) {
+    if (lvl < 1 || lvl > 100) {
+      return 'Level given must be between 1 and 100.';
+    }
+    const stats = this.getPrettyStats(pokemon);
+    return stats.InsertAt(lvl + ' ', pokemon.name.length + 1);
+  },
+  getValidMoveByLevel: function (pokemon, lvl) {
+    return pokemon.moves
+      .filter(move => move.level <= lvl)
+      .map(move => move.name);
+  },
+  evolvesWhen: function (pokemon) {
+    return pokemon.evolution.details
+      .filter(detail => detail.evolutionStep > pokemon.evolution.currentEvolutionStep)
+      .map(detail => `${detail.form.Capitalize()} - Level: ${detail.level || 'none'} -  Condition: ${detail.condition || 'none'}`)
+  },
+  evValue: function (pokemon, format) {
+    const val = pokemon.evYield
+      .map(ev => `${ev.type}: ${ev.value}`);
+
+    return format === 'string' ? val[0] : val;
+  }
 };
