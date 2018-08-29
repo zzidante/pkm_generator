29f50abee219792a74549e3a5637eea27fa64402
Mel <zzidante@gmail.com>
2018-08-28 23:12:09 -0700

    update pokemon moves format

--- pokemon_moves/1_pound.js
+++ pokemon_moves/1_pound.js
@@ -1,13 +1,19 @@
-module.exports = {
+module.exports = pound;
+
+const pound = {
   number: 1,
   name: 'pound',
+  rpg_description: `To hit accuracy roll must be be above a ${this.rpgStats.accuracy}. Roll a ${this.rpgStats.power}. It gains stab on ${this.type} pokemon. It is a ${this.category} attack and hits ${this.adjacentRules.allAdjacentFoes}.`,
   power: 40,
   accuracy: 100,
   rpgStats: {
     power: 'D4',
-    accuracy: 'D19',
+    accuracy: 'D1',
     statusEffect: false,
     multiHit: false,
+    target: function() { // probably have to look at bind for this?
+      
+    },
   },
   specialEffects: [],
   type: 'normal',
@@ -16,12 +22,12 @@ module.exports = {
   generation: 1,
   adjacentRules: {
     allAdjacent: false,
-    allAdjacentFoes: false,
+    allAdjacentFoes: true,
     all: false,
     allFoes: false,
     allAllies: false,
     targetOnly: true,
-    canHitAnyOneAdjacent: true,
+    canHitAnyOneAdjacent: false,
   },
   tm: false,
   hm: false,
