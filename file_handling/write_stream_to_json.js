97b96ce0132a75a4927c43a1f879bd0521e206e6
Mel <zzidante@gmail.com>
2021-06-08 01:51:40 -0700

    rename log to finishCallback as its non-specific

--- file_handling/write_stream_to_json.js
+++ file_handling/write_stream_to_json.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-function writeStreamToJson({fullyQualifiedPath, dataObject}, log) {
+function writeStreamToJson({fullyQualifiedPath, dataObject}, finishCallback) {
   const writeStream = fs.createWriteStream(fullyQualifiedPath);
   writeStream.write(JSON.stringify(dataObject, null, 2));
   writeStream.end();
   writeStream.on('finish', function () {
-    log();
+    finishCallback();
   });
 }
 
