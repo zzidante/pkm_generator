a73ec4045446e83621b569b2aca5fd3682f949ab
Mel <zzidante@gmail.com>
2021-06-08 09:04:55 -0700

    give helpful errors for when a user attempts to access a pokemon we haven't registered yet.

--- calculators/pull_pokemon_data/convert_to_tabletop_from_cli.js
+++ calculators/pull_pokemon_data/convert_to_tabletop_from_cli.js
@@ -2,7 +2,11 @@ const convertToTabletop = require('./convert_raw_data').default;
 const [_, pokemonName] = process.argv.slice(1);
 
 if (pokemonName) {
-  convertToTabletop(pokemonName);
+  try {
+    convertToTabletop(pokemonName);
+  } catch (e) {
+    console.log('That pokemon is not currently supported');
+  }
 } else {
   console.log('You must supply a valid pokemon name.');
 }
