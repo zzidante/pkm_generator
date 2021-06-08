d86efb230f608c3183d56333bec7ade94d199a12
Mel <zzidante@gmail.com>
2021-06-08 08:28:04 -0700

    add npm commands to run all functionality; install read-last-lines

---
+++ calculators/pull_pokemon_data/get_data_from_cli.js
@@ -0,0 +1,39 @@
+const getData = require('./get_data').default;
+const [_, pokemonName] = process.argv.slice(1);
+const readLastLines = require('read-last-lines');
+
+if (pokemonName) {
+  getData(pokemonName).then((qualifiedPath) => {
+    console.log(qualifiedPath);
+    const checkForFinishedFile = function (path, run = true) {
+      readLastLines.read(path, 1)
+      // hack for now until we are running everything on a server, by checking that
+      // the last line is a single non-prepadded ending bracket (and knowing our
+      // JSON is prettified) -- we can assume that once it exists the file is written.
+      .then((lines) => {
+        if (lines === '}') {
+          console.log('Write appears to have finished, ending the process');
+          process.exit();
+        } else {
+          // try it one last time
+          if (run) {
+            console.log("write didn't finish, check it again in 5 seconds");
+            setTimeout(function() {
+              checkForFinishedFile(path, false);
+            }, 5000);
+          } else {
+            console.log('write took longer than expected or is not ending as expected, manually abort.');
+          }
+        }
+      });
+    };
+
+    checkForFinishedFile(qualifiedPath);
+  }).catch((e) => {
+    // it's normal sometimes for some pokemon pages to become unavailable
+    // usually its a 503
+    console.log('Unexpected error', e.message);
+  });
+} else {
+  console.log('You must supply a valid pokemon name.');
+}
