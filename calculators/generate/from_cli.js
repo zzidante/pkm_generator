398185183f473233c45e96d255f680936fb1f3de
Mel <zzidante@gmail.com>
2021-06-08 01:49:11 -0700

    remove top level console.log not needed

--- calculators/generate/from_cli.js
+++ calculators/generate/from_cli.js
@@ -2,7 +2,7 @@ const generate = require('./pokemon_to_json_file').default;
 const [pokemonName, requestedLevel] = process.argv.slice(2);
 
 if (pokemonName && requestedLevel) {
-  console.log(generate([pokemonName, requestedLevel]));
+  generate([pokemonName, requestedLevel]);
 } else {
   console.log('You must supply a valid pokemon name and level when running this command.');
 }
