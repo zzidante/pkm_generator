d2e9fd83f7097af9a870c2fb7bc7505b3660159f
Mel <zzidante@gmail.com>
2021-06-08 09:03:35 -0700

    give helpful errors for when a user attempts to access a pokemon we haven't registered yet

--- calculators/generate/from_cli.js
+++ calculators/generate/from_cli.js
@@ -1,8 +1,11 @@
 const generate = require('./pokemon_to_json_file').default;
 const [pokemonName, requestedLevel] = process.argv.slice(2);
-
 if (pokemonName && requestedLevel) {
-  generate([pokemonName, requestedLevel]);
+  try {
+    generate([pokemonName, requestedLevel]);
+  } catch(e) {
+    console.log(`The pokemon ${pokemonName} is currently not supported in the static data set. Have you tried Bulbasaur?`);
+  }
 } else {
   console.log('You must supply a valid pokemon name and level when running this command.');
 }
