6f5a74d6cbaa19f3d88420b6b3cc6d6e501c3432
Mel <zzidante@gmail.com>
2021-06-07 21:43:17 -0700

    Add comment for clarity

--- calculators/filters/minify_stats.js
+++ calculators/filters/minify_stats.js
@@ -1,8 +1,9 @@
+// shrink the relevant stats into smaller derivatives
 module.exports = function minifyStats(stat_blob) {
   for(let stat in stat_blob) {
     if (stat !== 'level' && stat !== 'name') {
       stat_blob[stat] = Math.ceil(stat_blob[stat] / 10);
     }
   }
-  return stat_blob;  
+  return stat_blob;
 };
